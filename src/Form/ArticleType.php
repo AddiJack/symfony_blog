e01881ca35ff79415a2dae2dc52659f9b0650f56
Adeline Jacquot <ade.jacquot@gmail.com>
2019-05-28 15:23:44 +0200

    quete13

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ArticleType extends AbstractType
 {
@@ -14,8 +16,13 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('content')
-            ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('category', null, ['choice_label' => 'name']);
+        $builder->add('tags', EntityType::class, [
+            'class' => Tag::class,
+            'choice_label' => 'name',
+            'expanded' => True,
+            'multiple' => True,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
